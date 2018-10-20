558d43f260b167206e9fa10efad56218827bc4fc
Anand H <anand.h1729@gmail.com>
2018-10-20 21:05:09 +0530

    Improved String input using list

--- pallindrome.py
+++ pallindrome.py
@@ -1,57 +1,19 @@
-a1=str(input())
-
-a2=str(input())
-
-a3=str(input())
-
-a4=str(input())
-
-a5=str(input())
-
-a6=str(input())
-
-a7=str(input())
-
-a8=str(input())
-
-a9=str(input())
-
-a10=str(input())
-
-a11=str(input())
-
-a12=str(input())
-
-b1=str(a1+a2+a3)
-
-b2=str(a4+a5+a6)
-
-b3=str(a7+a8+a9)
-
-b4=str(a10+a11+a12)
-
-def reverse(s):
-
-   return s[::-1]
-
-def isPalindrome(s):
- 
-    rev = reverse(s)
- 
-    if (s == rev):
- 
-        print("It is a pallindrome")
-
-    else:
-        
-         print("Not a pallindrome")
-        
-
-
-isPalindrome(b1)
-
-isPalindrome(b2)
-
-isPalindrome(b3)
-
-isPalindrome(b4)
+a=list() 
+for x in range(12):
+	n=input("Enter a string : ")
+	#this adds 12 strings to the list a
+	a.append(str(n))
+b=""#declaring an empty string to concatenate 3 at a time
+for y in range(12):
+	b=b+a[y]
+	if((y+1)%3==0): 
+		#concatenates 3 at a time
+		word=b		
+		print("Real Word : ",word)
+		revword=b[::-1]
+		print("New Word : ",revword) 
+		if(word==revword):
+			print("The Concatenated words produce a Palindrome")
+		else:
+			print("The Concatenated words do not produce a Palindrome")
+		b=""
